8e76d095022fec3feab64fb788ee93bbe89277e6
Anya Vigolajnen <vigolajnen@gmail.com>
2021-02-14 12:06:22 +0300

    фоны

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -1,5 +1,17 @@
 export default () => {
   window.addEventListener(`load`, function () {
     document.body.classList.add(`load`);
+
+    let linksMenu = document.querySelector('.js-menu');
+    let box = document.querySelector('.animation-box');
+    linksMenu.addEventListener('click', (e) => {
+      let target = e.target;
+      let dataHref = target.getAttribute("data-href");
+      if (dataHref === 'prizes') {
+        box.classList.add('active');
+      } else {
+        box.classList.remove('active');
+      }
+    })
   });
 };

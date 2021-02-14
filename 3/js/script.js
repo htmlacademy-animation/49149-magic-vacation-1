42d95a097f454eb5167c7210ae1d397f181a0074
Keks <keks@htmlacademy.ru>
2021-02-15 01:15:45 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/script.js
+++ 3/js/script.js
@@ -10120,17 +10120,22 @@ __webpack_require__.r(__webpack_exports__);
   window.addEventListener(`load`, function () {
     document.body.classList.add(`load`);
 
-    let linksMenu = document.querySelector('.js-menu');
-    let box = document.querySelector('.animation-box');
-    linksMenu.addEventListener('click', (e) => {
+    let linksMenu = document.querySelector(`.js-menu`);
+    let box = document.querySelector(`.animation-box`);
+
+    linksMenu.addEventListener(`click`, (e) => {
       let target = e.target;
-      let dataHref = target.getAttribute("data-href");
-      if (dataHref === 'prizes') {
-        box.classList.add('active');
-      } else {
-        box.classList.remove('active');
+      let dataHref = target.getAttribute(`data-href`);
+      if (dataHref === `prizes`) {
+        e.preventDefault();
+        box.classList.add(`active`);
+
+        setTimeout(() => {
+          box.classList.remove(`active`);
+          window.location.hash = `#prizes`;
+        }, 1000);
       }
-    })
+    });
   });
 });
 
